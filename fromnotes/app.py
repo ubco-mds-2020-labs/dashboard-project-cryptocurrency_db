61bc85e5f8c0cdff5a18062d1198a3c34e82ec5f
afreenshakeel24 <afreen.ubc@gmail.com>
2021-03-10 01:17:07 -0800

    rework

--- fromnotes/app.py
+++ fromnotes/app.py
@@ -1,21 +1,29 @@
-#import dash
-#import dash_html_components as html
-
-
-#app = dash.Dash()
-#app.layout = html.Div('I am alive!!')
-#app.run_server()
-
-
+import altair as alt
 import dash
+import dash_core_components as dcc
 import dash_html_components as html
+from vega_datasets import data
+import pandas as pd
+import os
+
+os.chdir("/Volumes/UBC/Block5/551/Project_MDS/dashboard-project-cryptocurrency_db")
+source2 = pd.read_csv("price.csv")
+source2['New_date1']=pd.to_datetime(source2['New_date1'], format='%Y-%m-%d')
+
+line_volume = alt.Chart(source2).mark_line().encode(
+    x=alt.X('New_date1', sort='x'),
+    y=alt.Y('Volume'),
+    color='Name',
+    tooltip=['Name', 'Volume']
+).properties(
+    width=1400,
+    height=300
+)
 
 
 app = dash.Dash(__name__)
-
-app.layout = html.Div('I am alive finally!!')
+app.layout = html.Div([
+        html.Iframe(srcDoc=line_volume.to_html())])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-
-    
+    app.run_server(debug=True) 
