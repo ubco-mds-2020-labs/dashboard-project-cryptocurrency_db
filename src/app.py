b2cada0267c322c273a8b3fe29e3101c454d35f3
afreenshakeel24 <afreen.ubc@gmail.com>
2021-03-11 03:12:07 -0800

    dashboard

--- src/app.py
+++ src/app.py
@@ -10,13 +10,10 @@ import dash_bootstrap_components as dbc
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # Read in global data
-source1 = pd.read_csv("/Volumes/UBC/Block5/551/Project_MDS/dashboard-project-cryptocurrency_db/price.csv", index_col=0)
-crypto_usd_df = pd.read_csv("/Volumes/UBC/Block5/551/Project_MDS/dashboard-project-cryptocurrency_db/raw_data/crypto_usd.csv")
-usd_exchange_rate_df = pd.read_csv("/Volumes/UBC/Block5/551/Project_MDS/dashboard-project-cryptocurrency_db/raw_data/usd_exchange_rate.csv")
-source3 = pd.read_csv("/Volumes/UBC/Block5/551/Project_MDS/dashboard-project-cryptocurrency_db/data3.csv")
-#source3 = pd.read_csv("/Volumes/UBC/Block5/551/Project_MDS/dashboard-project-cryptocurrency_db/ExchangeRate.csv")
-#source3 = source3[['From Currency','To Currency', 'ExchangeRate']]
-ratings = source1['Name'].unique()
+source1 = pd.read_csv("/Volumes/UBC/Block5/551/Project_MDS/dashboard-project-cryptocurrency_db/data/processed_data/price.csv", index_col=0)
+crypto_usd_df = pd.read_csv("/Volumes/UBC/Block5/551/Project_MDS/dashboard-project-cryptocurrency_db/data/raw_data/crypto_usd.csv")
+usd_exchange_rate_df = pd.read_csv("/Volumes/UBC/Block5/551/Project_MDS/dashboard-project-cryptocurrency_db/data/raw_data/usd_exchange_rate.csv")
+source3 = pd.read_csv("/Volumes/UBC/Block5/551/Project_MDS/dashboard-project-cryptocurrency_db/data/processed_data/data3.csv")
 
 crypto_usd = crypto_usd_df.set_index('CRYPTOCURRENCY').T.to_dict('list')
 crypto_usd = {k:v[0] for k,v in crypto_usd.items()}
@@ -26,7 +23,7 @@ usd_exchange_rate = {k:v[0] for k,v in usd_exchange_rate.items()}
 
 
 # Setup app and layout/frontend
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__)
 
 
 
